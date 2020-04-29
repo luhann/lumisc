b43b68c4e3f32ea38215df771729a2dd16d8ae2c
Luke Hannan <luke@lukehannan.com>
2020-04-29 16:15:53 +0200

    Updated estimate_plot and added tests for it

---
+++ tests/testthat/test-plots.R
@@ -0,0 +1,10 @@
+context("plots")
+
+test_that("plot is created without error", {
+  x = lm(wt ~ mpg, data = mtcars)
+  y = lm(wt ~ disp, data = mtcars)
+
+  plot = estimate_plot(list(x, y), c("x", "y"))
+
+  expect_is(plot, "ggplot")
+})
