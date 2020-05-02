ae2692f470530884582376317f6f742c1a69496e
Luke Hannan <luke@lukehannan.com>
2020-05-02 17:42:35 +0200

    Updated some tests for R-4.0

--- tests/testthat/test-startup.R
+++ tests/testthat/test-startup.R
@@ -1,9 +1,11 @@
 context("r startup")
 
 test_that("correct startup options are set", {
-  defaults = options()
   set_startup_options()
-  startup = options()
-  expect_equal(names(startup), names(defaults))
+  startup <- options()
+  expect_true(startup[["warnPartialMatchArgs"]])
+  expect_true(startup[["warnPartialMatchAttr"]])
+  expect_true(startup[["warnPartialMatchDollar"]])
+  expect_false(startup[["show.signif.stars"]])
   expect_silent(set_startup_options())
 })
