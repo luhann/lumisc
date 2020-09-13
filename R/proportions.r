08e68715be490312abcfc977cee4544deafab289
Luke Hannan <luke@lukehannan.com>
2020-09-13 23:35:40 +0200

    Added diversity function

--- R/proportions.r
+++ R/proportions.r
@@ -28,3 +28,36 @@ return_proportions = function(dataframe, variable, precision = 2) {
 
   return(out)
 }
+
+#' Diversity
+#'
+#' Calculate a number of diversity indices
+#'
+#' This function assumes that rows are independent samples and columns are counts of differing species. For some
+#' diversity measures the input matrix must be positive and finite, if the output is NaN this is likely the issue.
+#'
+#' @name diversity
+#' @param matrix The matrix containing counts
+#' @param index Which diversity index to calculat
+#' @return A vector of indices for each row of the matrix provided
+#' @export
+diversity = function(matrix, index = "shannon") {
+  if (!is.matrix(matrix)) {
+    rlang::inform("Input not a matrix, attempting to coerce to matrix.", class = "coercion")
+
+    if (is.vector(matrix)) {
+      dim(matrix) = c(1, length(matrix))
+    } else {
+      matrix = as.matrix(matrix)
+    }
+  }
+
+  if (!is.numeric(matrix)) {
+    rlang::abort("Input matrix must be numeric.", class = "invalid_type_error")
+  }
+
+  if (index == "shannon") {
+    prop = matrix / rowSums(matrix)
+    -rowSums(prop * log(prop, base = exp(1)), na.rm = TRUE)
+  }
+}
--- tests/testthat/test-proportions.R
+++ tests/testthat/test-proportions.R
@@ -1,18 +1,25 @@
 context("proportions functions")
 
-
-
 test_that("fails with correct error", {
   data = data.frame(id = rep(1:10, times = 2L), count = rep(1:5, times = 4L))
+  char = matrix(rep(c("a", "b", "c"), times = 3), nrow = 3)
 
   expect_error(return_proportions(data, NULL), "length zero$")
   expect_error(return_proportions(data, "no_in"), class = "invalid_index_error")
   expect_error(return_proportions(data, 3L), class = "invalid_index_error")
+
+  expect_error(diversity(char, index = "shannon"), class = "invalid_type_error")
+})
+
+test_that("displays correct warnings", {
+  expect_message(diversity(1:9, index = "shannon"), regexp = "coerce to matrix")
 })
 
 test_that("returns correct result", {
   data = data.frame(id = rep(1:10, times = 2L), count = rep(1:5, times = 4L))
+  num_matrix = matrix(1, nrow = 1, ncol = 3)
 
   expect_equal(nrow(return_proportions(data, "count")), 6L)
   expect_equal(nrow(return_proportions(data, NA)), 1L)
+  expect_equal(diversity(num_matrix, index = "shannon"), -sum(num_matrix / rowSums(num_matrix) * log(num_matrix / rowSums(num_matrix))))
 })
