34c059b61282395966385b0a5046ec9145a40501
Luke Hannan <luke@lukehannan.com>
2025-01-21 19:21:56 +0200

    Add personal ggplot2 theme

--- R/plots.r
+++ R/plots.r
@@ -36,3 +36,51 @@ estimate_plot = function(model_list, model_names, coefficient = NULL) {
 
   invisible(p)
 }
+
+
+#' Tufte Maximal Data, Minimal Ink Theme With a Twist
+#'
+#' Theme based on Chapter 6 'Data-Ink Maximization and Graphical
+#' Design' of Edward Tufte *The Visual Display of Quantitative
+#' Information*. No border, no axis lines, no grids. This theme works
+#' best in combination with \code{geom_rug} or
+#' \code{geom_rangeframe}.
+#'
+#' @note
+#' The default font family is set to 'serif' as he uses serif fonts
+#' for labels in 'The Visual Display of Quantitative Information'.
+#' The serif font used by Tufte in his books is a variant of Bembo,
+#' while the sans serif font is Gill Sans. If these fonts are
+#' installed on your system, then you can use them with the package
+#' \bold{extrafont}.
+#'
+#' @inheritParams ggplot2::theme_minimal
+#' @param ticks \code{logical} Show axis ticks?
+#'
+#' @references Tufte, Edward R. (2001) The Visual Display of
+#' Quantitative Information, Chapter 6.
+#'
+#' @export
+theme_patroclus = function(base_size = 11, base_family = "serif", ticks = TRUE) {
+  check_package("ggplot2")
+
+  ret = ggplot2::theme_minimal(base_family = base_family, base_size = base_size) +
+    ggplot2::theme(
+      legend.background = ggplot2::element_blank(),
+      legend.key = ggplot2::element_blank(),
+      legend.position = "bottom",
+      legend.box = "vertical",
+      panel.background = ggplot2::element_blank(),
+      panel.border = ggplot2::element_blank(),
+      panel.grid = ggplot2::element_blank(),
+      strip.background = ggplot2::element_blank(),
+      plot.background = ggplot2::element_blank(),
+      axis.title.x = ggplot2::element_text(margin = ggplot2::margin(t = 10)),
+      axis.line = ggplot2::element_blank(),
+      axis.line.x = ggplot2::element_line(linewidth = 0.1, lineend = "butt")
+    )
+  if (!ticks) {
+    ret = ret + ggplot2::theme(axis.ticks = ggplot2::element_blank())
+  }
+  ret
+}
