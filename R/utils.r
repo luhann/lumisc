5df7137fa2564d4c85f0d189e0acfc762d8bdbd8
Luke Hannan <luke@lukehannan.com>
2026-06-09 16:33:12 +0200

    Update to latest Roxygen version

--- R/utils.r
+++ R/utils.r
@@ -88,33 +88,31 @@ q = function(save = "no", ...) {
   quit(save = save, ...)
 }
 
-
-#' view
+#' Return vector as quoted character vector
 #'
-#' Invoke a spreadsheet-style data viewer on a matrix-like R object.
+#' Take an existing R vector and add quotes to it for easy manipulation and copy/pasting.
 #'
-#' @name view
-#' @param x An R object which can be coerced to a data frame with non-zero numbers of rows and columns.
-#' @param title Title for viewer window. Defaults to name of 'x' prefixed by 'Data:'.
+#' @param vec A character or integer vector to be quoted
+#' @param collapse An optional character string to separate the results. Not NA_character_.
+#' @return An quoted vector of length \code{vec}.
+#'
 #' @export
-view = function(x, title) {
-  if (interactive()) {
-    utils::View(x, title)
-  }
+quote_vec = function(vec, collapse = ", ") {
+  paste('"', vec, '"', collapse = collapse, sep = "")
 }
 
-#' view_last
+
+#' Return hex value of specified rgb colour
 #'
-#' Invoke a spreadsheet-style data viewer on the last R object interacted with.
+#' Take an RGB colour value and return a hex colour code
 #'
-#' @name view_last
-#' @param title Title for viewer window. Defaults to name of 'x' prefixed by 'Data:'.
+#' @param r R channel
+#' @param g G channel
+#' @param b B channel
+#' @return A hex colour code
+#'
 #' @export
-view_last = function(title) {
-  if (interactive()) {
-    utils::View(.Last.value, title)
-  }
-}
+rgb2hex = function(r, g, b) grDevices::rgb(r, g, b, maxColorValue = 255)
 
 #' switchv
 #'
@@ -146,40 +144,29 @@ switchv = function(EXPR, ...) {
   result
 }
 
-#' create_list
+#' view
 #'
-#' Create an empty list that is length \code{names} and with each element named by the vector of names provided. This is
-#' shorthand for creating an empty list and then assigning it names.
+#' Invoke a spreadsheet-style data viewer on a matrix-like R object.
 #'
-#' @param names A character or integer vector of names to be assigned to list.
-#' @return An empty named list of length \code{names}.
-#'
+#' @name view
+#' @param x An R object which can be coerced to a data frame with non-zero numbers of rows and columns.
+#' @param title Title for viewer window. Defaults to name of 'x' prefixed by 'Data:'.
 #' @export
-create_list = function(names) {
-  stats::setNames(vector(mode = "list", length = length(names)), names)
+view = function(x, title) {
+  if (interactive()) {
+    utils::View(x, title)
+  }
 }
 
-#' Return vector as quoted character vector
+#' view_last
 #'
-#' Take an existing R vector and add quotes to it for easy manipulation and copy/pasting.
+#' Invoke a spreadsheet-style data viewer on the last R object interacted with.
 #'
-#' @param vec A character or integer vector to be quoted
-#' @param collapse An optional character string to separate the results. Not NA_character_.
-#' @return An quoted vector of length \code{vec}.
-#'
+#' @name view_last
+#' @param title Title for viewer window. Defaults to name of 'x' prefixed by 'Data:'.
 #' @export
-quote_vec = function(vec, collapse = ", ") {
-  paste('"', vec, '"', collapse = collapse, sep = "")
+view_last = function(title) {
+  if (interactive()) {
+    utils::View(.Last.value, title)
+  }
 }
-
-#' Return hex value of specified rgb colour
-#'
-#' Take an RGB colour value and return a hex colour code
-#'
-#' @param r R channel
-#' @param g G channel
-#' @param b B channel
-#' @return A hex colour code
-#'
-#' @export
-rgb2hex = function(r, g, b) grDevices::rgb(r, g, b, maxColorValue = 255)
