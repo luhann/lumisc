acb7f4dc6045ec7af0a2b294d17656bfe1223d36
Luke Hannan <luke@lukehannan.com>
2026-09-14 12:17:02 +0200

    Write write_df output with fwrite

--- R/dataframes.r
+++ R/dataframes.r
@@ -94,32 +94,21 @@ proportions.data.frame = function(x, margin = NULL, ...) {
 
 #' Write Dataframes
 #'
-#' Write a list of rectangular dataframes to a single csv keeping column names, and appending a single space between
-#' dataframes.
+#' Write a list of rectangular dataframes to a single csv keeping column names, separating dataframes with a blank line.
 #'
 #' This function never writes row names to the csv file.
 #'
 #' @name write_df
 #' @param list The list of dataframes to append to a single csv file.
 #' @param file File name of the final csv.
-#' @param overwrite Boolean value, if true will always overwrite file even if it exists.
-#' @param ... Additional options passed to the write.table command.
+#' @param overwrite Boolean value, if true will always overwrite file even if it exists, otherwise appends to it.
+#' @param ... Additional options passed to [data.table::fwrite()].
 #' @export
 write_df = function(list, file, overwrite = TRUE, ...) {
-  for (i in seq_len(length(list))) {
-    # on the first write we check if overwrite is true and overwrite the file if it exists
-    if (i == 1 && overwrite) {
-      utils::write.table(list[[i]], file, row.names = FALSE, col.names = TRUE, sep = ",", append = FALSE, ...)
-      utils::write.table("", file, row.names = FALSE, col.names = FALSE, sep = ",", append = TRUE, ...)
-      next
+  for (i in seq_along(list)) {
+    if (i > 1L) {
+      write("", file, append = TRUE)
     }
-
-    if (i == length(list)) {
-      utils::write.table(list[[i]], file, row.names = FALSE, col.names = TRUE, sep = ",", append = TRUE, ...)
-      next
-    }
-
-    utils::write.table(list[[i]], file, row.names = FALSE, col.names = TRUE, sep = ",", append = TRUE, ...)
-    utils::write.table("", file, row.names = FALSE, col.names = FALSE, sep = ",", append = TRUE, ...)
+    data.table::fwrite(list[[i]], file, append = i > 1L || !overwrite, col.names = TRUE, ...)
   }
 }
