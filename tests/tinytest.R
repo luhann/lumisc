e7428f108f5e2f2d5290b7d064e2d568935848c7
Luke Hannan <luke@lukehannan.com>
2026-09-14 12:17:02 +0200

    Remove hostname check from test runner

--- tests/tinytest.R
+++ tests/tinytest.R
@@ -1,4 +1,3 @@
 if (requireNamespace("tinytest", quietly = TRUE)) {
-  home = Sys.info()["nodename"] == "replicant"
-  tinytest::test_package("lumisc", at_home = home)
+  tinytest::test_package("lumisc")
 }
